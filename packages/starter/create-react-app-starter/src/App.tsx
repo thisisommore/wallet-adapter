8ae51c42e91308df7a4f97ac58250a799a5a00eb
thisisommore <ommore501@gmail.com>
2023-02-18 17:52:58 +0000

    Add update and decrement function

--- packages/starter/create-react-app-starter/src/App.tsx
+++ packages/starter/create-react-app-starter/src/App.tsx
@@ -3,8 +3,8 @@ import { ConnectionProvider, WalletProvider, useAnchorWallet } from '@solana/wal
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { UnsafeBurnerWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl, Connection } from '@solana/web3.js';
-import { Program, Provider, web3, BN } from '@project-serum/anchor'; //make sure to update dependency in package.json file
-import React, { FC, ReactNode, useMemo } from 'react';
+import { Program, web3, BN, Provider, AnchorProvider } from '@project-serum/anchor'; //make sure to update dependency in package.json file
+import React, { FC, ReactNode, useMemo, useState } from 'react';
 import idl from './idl.json';
 
 require('./App.css');
@@ -21,25 +21,13 @@ export default App;
 
 const Context: FC<{ children: ReactNode }> = ({ children }) => {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.testnet;
+    const network = WalletAdapterNetwork.Testnet;
 
     // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
     const wallets = useMemo(
         () => [
-            /**
-             * Wallets that implement either of these standards will be available automatically.
-             *
-             *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
-             *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-             *   - Solana Wallet Standard
-             *     (https://github.com/solana-labs/wallet-standard)
-             *
-             * If you wish to support a wallet that supports neither of those standards,
-             * instantiate its legacy wallet adapter here. Common legacy adapters can be found
-             * in the npm package `@solana/wallet-adapter-wallets`.
-             */
             new PhantomWalletAdapter(),
             new SolflareWalletAdapter({network}),
             new TorusWalletAdapter(),
@@ -60,22 +48,25 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 
 const Content: FC = () => {
     const wallet = useAnchorWallet();
-    const baseAccount = web3.Keypair.generate();
+    const [baseAccount] = useState(web3.Keypair.generate());
     
     function getProvider() {
         if (!wallet) {
             return null;
         }
+        
      /* create the provider and return it to the caller */
      /* network set to local network for now */
-        const network = "   "; //Enter localhost id here
+        const network = "http://localhost:8899"; //Enter localhost id here
         const connection = new Connection(network, "processed");
-        
-        const provider = new Provider(
-            connection, wallet, {"preflightCommitment": "prpcessed"},
+        const provider = new AnchorProvider(
+            connection, wallet, {preflightCommitment:"processed"},
         );
         return provider;
     }
+
+    const [modalActive,setModalActive]=useState(false)
+    const [value,setValue]= useState(0)
     
     async function createCounter() {
         const provider = getProvider()
@@ -89,8 +80,8 @@ const Content: FC = () => {
         
         const a = JSON.stringify(idl);
         const b = JSON.parse(a);
-        const program = new program(b, idk.metadata,address, provider);
-        
+        const program = new Program(b, idl.metadata.address, provider);
+        console.log(baseAccount.publicKey.toBase58());
         try {
             //interact with the program via rpc
             await program.rpc.initialize({
@@ -102,8 +93,7 @@ const Content: FC = () => {
                 signers: [baseAccount]
             });
             
-            
-            const account = await program.account.myAccount.fetch(baseAccount.publicKey);
+            const account = await program.account.counterData.fetch(baseAccount.publicKey);
             console.log('account: ', account);
         } catch(err) {
           console.log("Transaction error:", err);  
@@ -122,7 +112,7 @@ const Content: FC = () => {
         
         const a = JSON.stringify(idl);
         const b = JSON.parse(a);
-        const program = new program(b, idk.metadata,address, provider);
+        const program = new Program(b, idl.metadata.address, provider);
         
         try {
             //interact with the program via rpc
@@ -131,10 +121,72 @@ const Content: FC = () => {
                     myAccount: baseAccount.publicKey,
                 },
             });
+        console.log(baseAccount.publicKey.toBase58());
             
             
-            const account = await program.account.myAccount.fetch(baseAccount.publicKey);
-            console.log('account data: ', account.data.toString());
+            const account = await program.account.counterData.fetch(baseAccount.publicKey);
+            console.log('account data: ', (account as any).value.toString());
+        } catch(err) {
+          console.log("Transaction error:", err);  
+        }
+    }
+
+    async function decrement() {
+        const provider = getProvider()
+        
+        if(!provider) {
+            throw("Provider is null");
+        }
+        // create the program interface combining the idl, program ID, and provider
+        
+        //Bug with default importing when handling string value types. Fix it by reconverting to json
+        
+        const a = JSON.stringify(idl);
+        const b = JSON.parse(a);
+        const program = new Program(b, idl.metadata.address, provider);
+        
+        try {
+            //interact with the program via rpc
+            await program.rpc.decrement({
+                accounts: {
+                    myAccount: baseAccount.publicKey,
+                },
+            });
+            
+            
+            const account = await program.account.counterData.fetch(baseAccount.publicKey);
+            console.log('account data: ', (account as any).value.toString());
+        } catch(err) {
+          console.log("Transaction error:", err);  
+        }
+    }
+
+    async function update() {
+        const provider = getProvider()
+        
+        if(!provider) {
+            throw("Provider is null");
+        }
+        // create the program interface combining the idl, program ID, and provider
+        
+        //Bug with default importing when handling string value types. Fix it by reconverting to json
+        
+        const a = JSON.stringify(idl);
+        const b = JSON.parse(a);
+        const program = new Program(b, idl.metadata.address, provider);
+        console.log(baseAccount.publicKey.toBase58());
+        
+        try {
+            //interact with the program via rpc
+            await program.rpc.update(value,{
+                accounts: {
+                    myAccount: baseAccount.publicKey,
+                },
+            });
+            
+            
+            const account = await program.account.counterData.fetch(baseAccount.publicKey);
+            console.log('account data: ', (account as any).value.toString());
         } catch(err) {
           console.log("Transaction error:", err);  
         }
@@ -144,10 +196,15 @@ const Content: FC = () => {
     
     return (
         <div className="App">
+           {modalActive&& <div className="modal">
+                <input type="number" value={value} placeholder='Value' onChange={(e)=>setValue(+e.target.value)}/>
+                <button onClick={update}>Update</button>
+                <button onClick={()=>setModalActive(false)}>Close</button>
+            </div>}
             <button onClick={createCounter}>Initialize</ button>
-            <button>Update</ button>
+            <button onClick={()=>setModalActive(true)}>Update</ button>
             <button onClick={increment}>Increment</ button>
-            <button>Decrement</ button>
+            <button onClick={decrement}>Decrement</ button>
             <WalletMultiButton />
         </div>
     );
